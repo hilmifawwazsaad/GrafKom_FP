43c092ae61316f9fcf726c3b85752cd62be0d77e
cilokmblenyek <ahmadfatihramadhani@gmail.com>
2024-12-22 01:38:09 +0700

    Update page.js

--- src/app/features/page.js
+++ src/app/features/page.js
@@ -7,7 +7,7 @@ const FeatureCard = ({ icon, title, description }) => {
   return (
     <div className="max-w-xs bg-white shadow-lg rounded-lg p-6 text-center hover:shadow-2xl transition duration-300">
       <div className="text-4xl text-blue-500 mb-4">{icon}</div>
-      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <h3 className="text-xl text-gray-800 font-semibold mb-2">{title}</h3>
       <p className="text-gray-600">{description}</p>
     </div>
   );
